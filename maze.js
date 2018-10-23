5e21d350e613ae5011417e7d17483e74e27c87e7
Taffeya-Dunkley <44068501+Taffeya-Dunkley@users.noreply.github.com>
2018-10-22 20:55:46 -0500

    Exercise 2

--- maze.js
+++ maze.js
@@ -1,8 +1,21 @@
-window.onload = function(){
+/*window.onload = function(){
 	
 	 wall = document.getElementById("boundary1");
 	
 	wall.onmouseover = function(){
 		wall.classList.add("youlose");
 	};
-};
+}; */
+window.onload = function(){
+
+	 walls = document.getElementById("maze").querySelectorAll(".boundary");
+
+	for (let i=0;i<walls.length;i++){
+		walls[i].addEventListener("mouseover", function(){
+		for (i=0;i<walls.length;i++){
+				walls[i].classList.add("youlose")
+			}
+					
+		});
+	}
+}
